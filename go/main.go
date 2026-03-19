151b5721e292135ac2a2c2072bd431ffcb3e63dd
huaxiong <huaxiongcool@gmail.com>
2026-03-19 11:01:08 +0800

    添加go调试例子

--- go/main.go
+++ go/main.go
@@ -1,9 +1,17 @@
 package main
 
-func main() {
-	println("Hello, World!")
+import "net/http"
+
+func AddHttpHandler(w http.ResponseWriter, r *http.Request) {
+	println("Received a request")
 	sum := add(3, 5)
 	println("The sum of 3 and 5 is:", sum)
+	w.Write([]byte("Hello, World!"))
+}
+
+func main() {
+	http.HandleFunc("/hello", AddHttpHandler)
+	http.ListenAndServe(":8080", nil)
 }
 
 func add(a, b int) int {
